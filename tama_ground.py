29a8e9b81519b38dcfc635ffa3ad23fbd4bdc3aa
ohnuki-ohnuki <ohnukihiroki8585@yahoo.co.jp>
2021-09-05 16:15:45 +0900

    secondまで実行可能

--- tama_ground.py
+++ tama_ground.py
@@ -21,9 +21,28 @@ class GroundNotifier(Scraper) :
         ground_time = "14:00~"
       if (count == 20):
         ground_time = "16:00~"
+      if (count == 22):
+        ground_time = "17:00~"
       if (count == 24):
         ground_time = "18:00~"
-      return ground_time
+      if (count == 26):
+        ground_time = "19:00~"
+      return (ground_time)
+
+    def park_name(self, nb) :
+      ground_name = ""
+      if (nb == 0):
+        ground_name = "諏訪南公園"
+      if (nb == 1):
+        ground_name = "一本杉公園"
+      if (nb == 2):
+        ground_name = "貝取南公園"
+      if (nb == 3):
+        ground_name = "関戸公園"
+      if (nb == 4):
+        ground_name = "諏訪北公園"
+      return (ground_name)
+
 
     def execute_main(self) :
       # TO_ADDRESS_1 = 'tk.cw.milds@gmail.com'
@@ -49,97 +68,59 @@ class GroundNotifier(Scraper) :
       self.click('#WeeklyAkiListCtrl_DayTypeCheckBoxList_7')
       self.click('#WeeklyAkiListCtrl_FilteringButton')
       self.click('#ykr31101 > div.panel-layout-aki.horizontal-block > div:nth-child(1) > table.table-calendar > tbody > tr:nth-child(1) > th:nth-child(3) > a')
-      time.sleep(3)
+      time.sleep(2)
       # ソースコードを取得
-      html_main1 = self.driver.page_source
+      html_main_first = self.driver.page_source
       # HTMLをパースする
-      soup1 = BeautifulSoup(html_main1, 'lxml') # または、'html.parser'
+      soup_first = BeautifulSoup(html_main_first, 'lxml') # または、'html.parser'
+      table_date_first = soup_first.find(class_='label-datechange-currentdate calendar-selected-date')
+      date_first = table_date_first.get_text()
 
-      table_date1 = soup1.find(class_='label-datechange-currentdate calendar-selected-date')
-      date1 = table_date1.get_text()
-
-      south_col2 = []
-      ippon_col = []
-      south_availability2 = []
-      ippon_availability = []
-      south_colcount2 = 0
-      ippon_colcount = 0
-      south_availability = []
       message = ""
+      for j in range(5):
+        table_first = soup_first.find_all(class_='calendar-datarow-day')[int(j)]
+        first_catch = table_first.find_all('td')
+        first_col = []
+        first_ava = []
+        first_count = 0
+
+        for i in range(1,len(first_catch) ):
+          first_catch = table_first.find_all('td')[int(i)]
+          first_col.append(int(first_catch["colspan"]))
+          first_ava.append(first_catch.get_text())
+          first_count = 0
+
+        for (ava,col) in zip(first_ava,first_col):
+          if (first_count >= 4 and first_count <= 26 and str(ava) == "空き"):
+            message += str(self.park_name(j)) + date_first + "  " + self.convert_count_to_time(first_count) + "空きあり" + "\n"
+          first_count += int(col)
+
+      self.click('#DailyAkiListCtrl_NextDayImgBtn')
+      time.sleep(2)
+      html_main_second = self.driver.page_source
+      soup_second = BeautifulSoup(html_main_second, 'lxml')
+      table_date_second = soup_second.find(class_='label-datechange-currentdate calendar-selected-date')
+      date_second = table_date_second.get_text()
 
       for j in range(5):
-        table_south = soup1.find_all(class_='calendar-datarow-day')[int(j)]
-        south_catch = table_south.find_all('td')
-        south_col = []
-        south_availability = []
-        south_count = 0
+        table_second = soup_second.find_all(class_='calendar-datarow-day')[int(j)]
+        second_catch = table_second.find_all('td')
+        second_col = []
+        second_ava = []
+        second_count = 0
 
-        for i in range(1,len(south_catch) ):
-          south_catch = table_south.find_all('td')[int(i)]
-          south_col.append(int(south_catch["colspan"]))
-          south_availability.append(south_catch.get_text())
-          south_count = 0
+        for i in range(1,len(second_catch) ):
+          second_catch = table_second.find_all('td')[int(i)]
+          second_col.append(int(second_catch["colspan"]))
+          second_ava.append(second_catch.get_text())
+          second_count = 0
 
-          for (ava,col) in zip(south_availability,south_col):
-            if (south_count >= 4 and south_count <= 26 and str(ava) == "空き"):
-              message += str(j) + date1 + "  " + self.convert_count_to_time(south_count) + "空きあり" + "\n"
-              south_count += int(col)
-            else:
-              south_count += int(col)
-              # print(south_count)
+        for (ava,col) in zip(second_ava,second_col):
+          if (second_count >= 4 and second_count <= 26 and str(ava) == "空き"):
+            message += str(self.park_name(j)) + date_second + "  " + self.convert_count_to_time(second_count) + "空きあり" + "\n"
+          second_count += int(col)
+      print(message)
 
-
-          print(str(ava) + ":" + str(col) + " total:" + str(south_count))
-
-      # print(message)
-
-      # for i in range(1,10):
-      #   try:
-      #     ippon_catch = table_ippon.find_all('td')[int(i)]
-      #     ippon_col.append(int(ippon_catch["colspan"]))
-      #     ippon_colcount += 1
-      #   except IndexError as ie:
-      #     break
-
-      # for i in range(1,ippon_colcount+1):
-      #   ippon_catch = table_ippon.find_all('td')[int(i)]
-      #   ippon_availability.append(ippon_catch.get_text())
-
-      # ippon_count = 0
-      # for (ava,col) in zip(ippon_availability,ippon_col):
-      #   if (ippon_count >= 4 and ippon_count <= 26 and str(ava) == "空き"):
-      #     print("一本杉公園" + date1 + "  " + self.convert_count_to_time(ippon_count) + "空きあり")
-      #   ippon_count += int(col)
-      #   # print(str(ava) + ":" + str(col) + " total:" + str(ippon_count))
-
-      # self.click('#DailyAkiListCtrl_NextWeekImgBtn')
-      # time.sleep(2)
-      # # ソースコードを取得
-      # html_main2 = self.driver.page_source
-      # # HTMLをパースする
-      # soup2 = BeautifulSoup(html_main2, 'lxml') # または、'html.parser'
-      # table_date2 = soup2.find(class_='label-datechange-currentdate calendar-selected-date')
-      # date2 = table_date2.get_text()
-
-      # table_south2 = soup2.find_all(class_='calendar-datarow-day')[0]
-      # for i in range(1,10):
-      #   try:
-      #     south_catch2 = table_south2.find_all('td')[int(i)]
-      #     south_col2.append(int(south_catch2["colspan"]))
-      #     south_colcount2 += 1
-      #   except IndexError as ie:
-      #     break
-
-      # for i in range(1,south_colcount2+1):
-      #   south_catch2 = table_south2.find_all('td')[int(i)]
-      #   south_availability2.append(south_catch2.get_text())
-
-      # south_count2 = 0
-      # for (ava,col) in zip(south_availability2,south_col2):
-      #   if (south_count2 >= 4 and south_count2 <= 26 and str(ava) == "空き"):
-      #     print("諏訪南公園" + date2 + "  " + self.convert_count_to_time(south_count2) + "空きあり")
-      #   south_count2 += int(col)
-      #   # print(str(ava) + ":" + str(col) + " total:" + str(south_count))
 
       # ブラウザを終了する
       self.driver.quit()
